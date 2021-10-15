1b11a80a78300b3abd78d81169848c8145e1f7fd
Ngô Minh Tú <41754738+NMT99@users.noreply.github.com>
2021-10-15 04:37:42 +0000

    Update config.fish

--- config.fish
+++ config.fish
@@ -3,6 +3,7 @@ if status is-interactive
 fortune | cowsay -f tux | lolcat
 #neofetch --disable cpu memory --ascii_distro archlinux
 set fish_greeting "Have a good day!"
+#fortune | cowsay -f (ls /usr/share/cows | shuf -n 1 | cut -f1 -d '.') | lolcat
 
 
 alias pycharm "Downloads/pycharm-community/bin/pycharm.sh"
