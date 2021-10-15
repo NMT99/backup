0c573da0b7c2352f07779a0cd4657080f62f7bcd
Ngô Minh Tú <41754738+NMT99@users.noreply.github.com>
2021-10-15 04:31:47 +0000

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+fortune | cowsay -f tux | lolcat
+#neofetch --disable cpu memory --ascii_distro archlinux
+set fish_greeting "Have a good day!"
+
+
+alias pycharm "Downloads/pycharm-community/bin/pycharm.sh"
+
+
+end
