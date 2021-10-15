17fb51ad14779282c3afca9df312a244cb2ffba9
Ngô Minh Tú <41754738+NMT99@users.noreply.github.com>
2021-10-15 04:42:20 +0000

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-fortune | cowsay -f (ls /usr/share/cows/ | shuf -n 1)
+fortune | cowsay -f (ls /usr/share/cows/ | shuf -n 1) | lolcat
 #fortune | cowsay -f tux | lolcat
 #neofetch --disable cpu memory --ascii_distro archlinux
 set fish_greeting "Have a good day!"
